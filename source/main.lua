fcb2d8c7cec2f65bc92dcde7ccfc3ed0db0d99ff
Mike Roibu <contact@mikeroibu.com>
2024-05-26 12:09:28 +0100

    Initial project setup (no tasks/build actions)

--- source/main.lua
+++ source/main.lua
@@ -1,1 +1,19 @@
-print("Hello world")
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+-- local gfx <const> = playdate.graphics
+
+local function myGameSetUp()
+    print("Game setup")
+
+end
+
+myGameSetUp()
+
+function playdate.update()
+
+    -- gfx.sprite.update()
+    playdate.timer.updateTimers()
+end
