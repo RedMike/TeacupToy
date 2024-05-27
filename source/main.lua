73f7d6376d432116ddf510b0a4d1606024787e8d
Mike Roibu <contact@mikeroibu.com>
2024-05-27 20:31:05 +0100

    Initial setup of assets

--- source/main.lua
+++ source/main.lua
@@ -3,17 +3,30 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
--- local gfx <const> = playdate.graphics
+local gfx <const> = playdate.graphics
+local tilemap
+local playerImg
+
+local playerSprite
 
 local function myGameSetUp()
-    print("Game setup")
+    print("Game setup start")
+    gfx.setImageDrawMode(gfx.kDrawModeBlackTransparent)
+    gfx.setBackgroundColor(gfx.kColorBlack);
 
+    tilemap = gfx.imagetable.new("images/tilemap")
+    playerImg = tilemap:getImage(1, 3)
+
+    playerSprite = gfx.sprite.new(playerImg)
+    playerSprite:moveTo(100, 100)
+    playerSprite:add()
+    print("Game setup end")
 end
 
 myGameSetUp()
 
 function playdate.update()
-
-    -- gfx.sprite.update()
+    gfx.clear()
+    gfx.sprite.update()
     playdate.timer.updateTimers()
 end
